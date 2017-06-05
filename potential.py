47523a1624acf538e54be3497f16f35bb4b7d49e
kaija <kaija.chang@gmail.com>
2017-06-05 23:10:45 +0800

    update stock

--- potential.py
+++ potential.py
@@ -14,22 +14,30 @@ def filter(data):
     del data['LP']
     del data['TC']
     WIND=5
+    hit = False
     total_len = len(data.index)
     data['tv_mean'] = data['TV'].rolling(WIND).mean().shift(1)
     #return(0)
     waves = []
+    waves_start_price = []
+    drop_counter = 0
     idx = 0
     for i, row in data.iterrows():
-        #if math.isnan(row['tv_mean']):
-        #    idx = idx + 1
-        #    continue
+        #print i
+        #print "TV:" + str(row['TV']) + " TV mean:" + str(row['tv_mean'])
+        #print "CR:" + str(row['CR'])
         if (row['TV'] > 2 * row['tv_mean'] and row['CR'] > 5) or row['OR'] == 10:
             #wave first day
-            print i
+            waves_start_price.append(row['CP'])
             waves.append(i)
             continue
+        if len(waves_start_price) > 0 and waves_start_price[-1] * 0.95 > row['CP']:
+            drop_counter = drop_counter  + 1
     print waves
-    print waves[-1]
+    print waves_start_price
+    if len(waves) > 0 and drop_counter == 0:
+        hit = True
+    return hit
     #print data
 
 def filter2(data, starts):
@@ -38,7 +46,10 @@ def filter2(data, starts):
 def main(days=30, sid=None, date_from=None):
     df = preprocess(days=days, sid=sid, date_from=date_from)
     #print df
-    filter(df)
+    if filter(df):
+        print sid + " GOTCHA"
+    else:
+        print "BYE"
 
 def preprocess(days=30, sid=None, date_from=None):
 
@@ -52,7 +63,7 @@ def preprocess(days=30, sid=None, date_from=None):
             df = dfo["20100101":fdate.strftime("%Y-%m-%d")]
             #df = dfo[]
         subset = df.tail(days + 1)
-        print subset
+        #print subset
         last_cp = 0
         #initial some rate filed
         df_r = subset[0:0]
